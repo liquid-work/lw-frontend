1a4dabce4fb9dd0be7ae6d50127bd9fe01603df1
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-05-11 09:34:27 -0700

    setup Moralis, .env file, frontpage cleanup

---
+++ constants.ts
@@ -0,0 +1,2 @@
+export const MORALIS_SERVER_URL = String(process.env.NEXT_PUBLIC_MORALIS_SERVER_URL);
+export const MORALIS_APP_ID = String(process.env.NEXT_PUBLIC_MORALIS_APP_ID);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { MoralisProvider } from "react-moralis";
+import { MORALIS_APP_ID, MORALIS_SERVER_URL } from "../constants";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <MoralisProvider serverUrl={MORALIS_SERVER_URL} appId={MORALIS_APP_ID}>
+    <Component {...pageProps} />
+  </MoralisProvider>
 }
 
-export default MyApp
+export default MyApp;
