bb69238e9e99e482e3c4275df887162968a9ea4c
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-05-11 09:43:40 -0700

    setup redux-toolkit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
 import { MoralisProvider } from "react-moralis";
 import { MORALIS_APP_ID, MORALIS_SERVER_URL } from "../constants";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <MoralisProvider serverUrl={MORALIS_SERVER_URL} appId={MORALIS_APP_ID}>
-    <Component {...pageProps} />
-  </MoralisProvider>
+  return <Provider store={store}>
+    <MoralisProvider serverUrl={MORALIS_SERVER_URL} appId={MORALIS_APP_ID}>
+      <Component {...pageProps} />
+    </MoralisProvider>
+  </Provider >
 }
 
 export default MyApp;
